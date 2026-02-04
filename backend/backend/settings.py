381bae92d0f70b27e2495f6c5379aee3bdef9b41
Olivier Kangah <kangaholivier22@gmail.com>
2026-02-04 17:17:16 +0000

    secret change

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -63,9 +63,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+
+
+SECRET_KEY = "django-insecure-%x#0wa&djs3q9)pg*#_%+-3sb7_3p(#1lzs6hun+py+rtn&ej="
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "192.168.1.13", "127.0.0.1", "backend.decrouresi.com", "decrouresi.com"]
 
